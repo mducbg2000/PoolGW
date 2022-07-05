6fc2762f60283ab5872471a68d082b45e817c926
Minh Đức <mducbg2000@gmail.com>
2022-07-05 23:00:19 +0700

    Fake WETH token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,9 @@
 const hre = require("hardhat");
 const ethers = hre.ethers;
 
+const weth9 = require("../abi/WETH9.json");
+const address = require("../address");
+
 async function main() {
   const PoolGW = await ethers.getContractFactory("PoolGW");
   const gw = await PoolGW.deploy();
@@ -13,6 +16,14 @@ async function main() {
   await gw.newGateway("Aave", aaveGw.address);
 
   console.log(await gw.getGatewayAddress("Aave"));
+
+  const [owner, user] = await ethers.getSigners();
+
+  // mint 100 weth token for user
+  const weth = new ethers.Contract(address.WETH, weth9, ethers.provider);
+  await weth.connect(user).deposit({
+    value: ethers.utils.parseEther("0.0000000000000001"),
+  });
 }
 
 main().catch((error) => {
