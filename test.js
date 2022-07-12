822bcfd10ac6260d857d19fa9e0d39ef403d62a2
Minh Đức <mducbg2000@gmail.com>
2022-07-12 13:08:57 +0700

    Add diagram

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-const IERC20 = require('./abi/IERC20.json')
-const address = require('./address')
-const [owner, user] = await ethers.getSigners()
+const IERC20 = require("./abi/IERC20.json");
+const address = require("./address");
+const [owner, user] = await ethers.getSigners();
 const bat = new ethers.Contract(address.BAT, IERC20, user);
-await bat.approve("0xAe120F0df055428E45b264E7794A18c54a2a3fAF", 20)
+const poolGw = await ethers.getContractAt(
+  "PoolGW",
+  "0x1c9fD50dF7a4f066884b58A05D91e4b55005876A"
+);
+await bat.approve("0xb185e9f6531ba9877741022c92ce858cdcc5760e", 200);
